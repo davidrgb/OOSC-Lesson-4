266ebc969621d6f660f7ddef616a0761a194624c
davidrgb <davidrussellrgb@gmail.com>
2021-03-22 22:10:15 -0500

    Finished GUI

--- view/MenuScreen.java
+++ view/MenuScreen.java
@@ -39,5 +39,13 @@ public class MenuScreen {
             window.pack();
             window.revalidate();
         });
+
+        drawingButton.addActionListener( event -> {
+            window.getContentPane().removeAll();
+            var menu = new DrawingDemoPanel(window);
+            menu.init();
+            window.pack();
+            window.revalidate();
+        });
     }
 }
